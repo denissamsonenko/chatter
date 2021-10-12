42064e0092cd04c5eba25500a711eda3994d63af
denissamsonenko <denissamsonenko@mail.ru>
2021-10-12 11:24:13 +0300

    added WEBpack and nd front split into modules

--- src/main/java/by/htp/chatter/controller/MainController.java
+++ src/main/java/by/htp/chatter/controller/MainController.java
@@ -3,6 +3,7 @@ package by.htp.chatter.controller;
 import by.htp.chatter.model.User;
 import by.htp.chatter.repositry.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,9 @@ import java.util.HashMap;
 public class MainController {
     private final MessageRepository messageRepository;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @Autowired
     public MainController(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
@@ -25,12 +29,14 @@ public class MainController {
     public String main(
             Model model,
             @AuthenticationPrincipal User user
-    ){
+    ) {
         HashMap<Object, Object> attributeValue = new HashMap<>();
         attributeValue.put("profile", user);
         attributeValue.put("messages", messageRepository.findAll());
 
         model.addAttribute("frontendData", attributeValue);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
         return "index";
     }
 }
